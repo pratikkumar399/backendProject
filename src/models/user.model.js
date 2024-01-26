02f3ed685f3e9376d98abf76f1e7b85cda06a91f
raipratik <pratikkumar399@gmail.com>
2024-01-26 23:47:34 +0530

    making user model

---
+++ src/models/user.model.js
@@ -0,0 +1,29 @@
+import mongoose, { Schema } from "mongoose";
+
+const userSchema = new Schema(
+    {
+        username: {
+            type: String,
+            required: true,
+            unique: true,
+            lowercase: true,
+            trim: true,
+            index: true
+        },
+        email: {
+            type: String,
+            required: true,
+            unique: true,
+            lowercase: true,
+            trim: true,
+
+        },
+        fullname: {
+            type: String,
+            required: true,
+            trim: true,
+        },
+    }
+)
+
+export const User = mongoose.model("User", userSchema);
