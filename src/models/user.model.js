066b1b876211542cf67f1a1ea2b68bc10348d979
raipratik <pratikkumar399@gmail.com>
2024-01-28 23:23:44 +0530

    configuring the password security

--- src/models/user.model.js
+++ src/models/user.model.js
@@ -54,10 +54,14 @@ const userSchema = new Schema(
 )
 
 userSchema.pre("save", async function (next) {
-
+    // if password is not modified, skip
     if (!this.isModified("password")) return next();
     this.password = await bcrypt.hash(this.password, 10);
+    next();
 })
 
+userSchema.methods.isPasswordCorrect = async function (password) {
+    return await bcrypt.compare(password, this.password);
+}
 
 export const User = mongoose.model("User", userSchema);
