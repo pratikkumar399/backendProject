2ecfc95ef9a78a1948f8a138f8b684577531acfa
raipratik <pratikkumar399@gmail.com>
2024-02-01 02:41:18 +0530

    all setup done

--- src/app.js
+++ src/app.js
@@ -13,5 +13,14 @@ app.use(express.urlencoded({ extended: true }));
 app.use(express.static('public'));
 app.use(cookieParser());
 
+// import routes
+
+import userRoutes from './routes/user.routes.js';
+
+
+// routes declaration
+app.use('/api/v1/users', userRoutes);
+
+
 
 export { app };
